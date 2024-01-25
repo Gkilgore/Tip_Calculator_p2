65778936c113155d5b0440f594c27fede3299e5d
Gkilgore <gkilgore@frhsd.com>
2024-01-25 13:20:31 -0500

    finished tutorial

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function calculateAndDisplay() {
         'Amount per person: $' + amtPerPers.toFixed(2)
 
 }
+// Helper function to calculate tip based on bill amount and service quality
+function calculateTip(bill, quality){
+   if (quality === 'Great'){
+     return bill * .2
+   } else if (quality === 'Good'){
+    return bill *.15
+   } else {
+    return bill * .10
+   }
+}
+
+function calculateTotalBill(bill, tip){
+   return bill + tip
+}
+
+function calcAmtPerPers(totalBill, numOfPpl){
+    return totalBill / numOfPpl
+}
